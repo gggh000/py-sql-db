754e9787099fb4726b89df1408e2e92c4f10bc37
Your Name <you@example.com>
2019-04-20 04:05:50 -0400

    py-sql-db.py: added.

---
+++ py-sql-db.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''
+PASSWORD AND PHONE MANAGEMENT SYSTEM.
+GUYEN GN.
+THIS UTILITY REQUIRES MYSQL SERVER RUNNING ON CONFIG_MYSQL_IP
+'''
+
+def printBarDouble():
+	print("=================================================")
+
+def printBarSingle():
+	print("-------------------------------------------------")
+
+import os
+import sys
+from cmnLib3 import *
+
+os.system("clear")
+
+printBarDouble()
+print("MAIN MENU")
+printBarDouble()
+
